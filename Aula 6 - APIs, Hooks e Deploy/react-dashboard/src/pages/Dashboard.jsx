79c85f41aca6d2fab911fe4685dc3e8b3caa9d83
augusto dev <augusto.valerio.dev@gmail.com>
2025-09-23 23:52:42 -0300

    aula-23/09/2025

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Dashboard.jsx
@@ -1,10 +1,18 @@
+import { GraficoBarras } from "@/components/GraficoBarras";
 import Tabs from "../components/Tabs";
+import { ChartPieSimple } from "@/components/ChartPieSimple.jsx";
 
 export default function Dashboard() {
   return (
-    <div className="p-6 flex-1 bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard</h1>
-      <Tabs />
-    </div>
+    <>
+      <div className="p-6 flex-1 bg-gray-100">
+        <h1 className="text-2xl font-bold mb-4">Bem-vindo ao Dashboard</h1>
+        <Tabs />
+      </div>
+      <div className="flex">
+        <ChartPieSimple />
+        <GraficoBarras />
+      </div>
+    </>
   );
 }
